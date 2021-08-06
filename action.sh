28be81a44ceea0c398f1b64054ae156993c49b4e
DunggVN <65992011+DunggVN@users.noreply.github.com>
2021-08-06 17:08:24 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
 branch=pruh
 git clone -b $branch https://github.com/AbOuLfOoOoOuF/ProjectFizilion /root/Forkzilion
-cp projectfizilion/config.env /root/Forkzilion/config.env
+cp forkzilion/config.env /root/Forkzilion/config.env
 cd /root/Forkzilion
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t projectfizilion
 docker run --privileged --env-file config.env --rm -i projectfizilion
