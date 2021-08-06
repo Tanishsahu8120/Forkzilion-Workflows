2e44d42acc65314bd2b5d023d4b7877448ef86f6
DunggVN <65992011+DunggVN@users.noreply.github.com>
2021-08-06 09:12:42 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
 branch=pruh
 git clone -b $branch https://github.com/AbOuLfOoOoOuF/ProjectFizilion /root/Forkzilion
-cp ultroid/.env /root/TeamUltroid/.env
-cd /root/TeamUltroid
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
-docker run --privileged --env-file .env --rm -i ultroid
+cp projectfizilion/.env /root/Forkzilion/.env
+cd /root/Forkzilion
+docker build . --rm --force-rm --compress --pull --file Dockerfile -t projectfizilion
+docker run --privileged --env-file .env --rm -i projectfizilion
